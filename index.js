e102034d54d459361c895803288cc060c579ca75
丁淳浩 <381590166@qq.com>
2019-12-09 15:18:34 +0800

    babel配置学习

---
+++ index.js
@@ -0,0 +1,18 @@
+import '@babel/polyfill';
+
+const fn=()=>{console.log('hello')}
+const isHas = [1,2,3].includes(2);
+
+const p = new Promise((resolve, reject) => {
+    resolve(100);
+});
+class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    };
+    getX() {
+      return this.x;
+    }
+  }
+  let cp = new ColorPoint(25, 8);
